c831c225efee5d77504ff01b2f6818f7db30d7a6
Minh Tri <minhtri24092005@gmail.com>
2025-09-29 15:07:56 +0700

    video31-obj value css

--- src/componets/learn/MyComponet.jsx
+++ src/componets/learn/MyComponet.jsx
@@ -6,12 +6,19 @@
 
 import './style.css';
 const MyComponent = () => {
+    const name = "Meow"; //String or //Number or //Boolean or //Array or //Object
+
+    const obj = {            //object
+        name: "love CAT",
+        age: 30
+    }
+
+    //JSON.stringify(obj)
+
     return (
         <>
-            <div className="child">eric & otiso love</div>
-            <div className="child">  eric & otiso love eric & otiso love
-                {/* style = {{ color: 'blue', fontSize: '30px' }} */}
-            </div>
+            <div className="child">{name} {obj.age} {obj.name} eric & otiso love</div>
+            <div className="child" style={{ color: "red" }}> eric & otiso love eric & otiso love</div>
         </>
     );
 }
