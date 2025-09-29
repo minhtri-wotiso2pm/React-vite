726271015e1bcb28aef14e254c412e0d1aa6c29a
Minh Tri <minhtri24092005@gmail.com>
2025-09-29 14:07:27 +0700

    video 30

--- src/componets/learn/MyComponet.jsx
+++ src/componets/learn/MyComponet.jsx
@@ -1,8 +1,18 @@
 // () => {}
 //component = html + js + css
+
+//JSX: 1 parent + many children
+
+
+import './style.css';
 const MyComponent = () => {
     return (
-        <div>eric & wotiso love</div>
+        <>
+            <div className="child">eric & otiso love</div>
+            <div className="child">  eric & otiso love eric & otiso love
+                {/* style = {{ color: 'blue', fontSize: '30px' }} */}
+            </div>
+        </>
     );
 }
 
