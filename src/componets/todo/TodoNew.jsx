38de1729fb8750d2c7caf5788cfd3c72671eee8e
Minh Tri <minhtri24092005@gmail.com>
2025-10-01 06:15:11 +0700

    video 36

--- src/componets/todo/TodoNew.jsx
+++ src/componets/todo/TodoNew.jsx
@@ -4,10 +4,24 @@ const TodoNew = (props) => {
     const { addNewTodo } = props;
 
     // addNewTodo("Eric");
+    const handleClick = () => {
+        alert("Click me");
+    }
+
+    const handleChange = (name) => {
+        console.log(">>> Handle Change", name);
+    }
+
     return (
         <div>
-            <input type="text" />
-            <button>Add</button>
+            <input type="text"
+                onChange={(event) => { handleChange(event.target.value) }} />
+                //onchange su kien khi nguoi dung nhap du lieu vao input
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}>
+                //su kien khi nguoi dung click vao button
+                Add</button>
         </div>
     )
 }
